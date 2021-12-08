4ebabca506641de29775305e437f9ed19e8f05a3
Seng Jik <853974536@qq.com>
2021-12-09 01:11:41 +0800

    SDL support

--- cpymo-backends/sdl2/main.c
+++ cpymo-backends/sdl2/main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <cpymo_error.h>
+#include <SDL.h>
 
-int main(void) {
+int main(int argc, char **argv) {
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_Window *win = SDL_CreateWindow("Cpymo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 768, 0);
 	printf("Hello, pymo!");
 	const char *msg = cpymo_error_message(CPYMO_ERR_SUCC);
 	printf("%s", msg);
+
+	SDL_Delay(5000);
+
+	SDL_DestroyWindow(win);
+	SDL_Quit();
 	return 0;
 }
