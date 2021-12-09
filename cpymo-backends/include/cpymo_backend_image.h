6b43e368a54af7566626c46fe7cff41e06079994
Seng Jik <853974536@qq.com>
2021-12-10 00:54:03 +0800

    Bug fixed.

--- cpymo-backends/include/cpymo_backend_image.h
+++ cpymo-backends/include/cpymo_backend_image.h
@@ -9,7 +9,7 @@ struct cpymo_backend_image;
 
 error_t cpymo_backend_image_load(cpymo_backend_image **out_image, const char *path);
 error_t cpymo_backend_image_move(cpymo_backend_image **out_image, const void *pixels, int width, int height, int channels);
-error_t cpymo_backend_image_font(cpymo_backend_image **out_image, utf8_int32_t codepoint);
+error_t cpymo_backend_image_font(cpymo_backend_image **out_image, const char *utf8str);
 error_t cpymo_backend_image_update(cpymo_backend_image *image, const void *pixels);
 
 void cpymo_backend_image_free(cpymo_backend_image *image);
@@ -24,13 +24,27 @@ int cpymo_backend_image_height(const cpymo_backend_image *img);
  * RightBottom - (gameconfig.imagesize.w - 1, gameconfig.imagesize.h - 1)
  */
 
-void cpymo_backend_image_render(
+
+enum cpymo_backend_image_draw_type {
+	cpymo_backend_image_draw_type_bg,
+	cpymo_backend_image_draw_type_chara,
+	cpymo_backend_image_draw_type_textbox,
+	cpymo_backend_image_draw_type_uielement,
+	cpymo_backend_image_draw_type_text_say,
+	cpymo_backend_image_draw_type_text_text,
+	cpymo_backend_image_draw_type_text_ui,
+	cpymo_backend_image_draw_type_effect
+};
+
+
+void cpymo_backend_image_draw(
 	const cpymo_backend_image *image,
 	float dstx, float dsty, float dstw, float dsth,
-	int srcx, int srcy, int srcw, int srch, float alpha);
+	int srcx, int srcy, int srcw, int srch, float alpha,
+	cpymo_backend_image_draw_type draw_type);
 
 
-void cpymo_backend_image_fillscreen(
+void cpymo_backend_image_fillcolor(
 	cpymo_color col,
 	float alpha);
 
