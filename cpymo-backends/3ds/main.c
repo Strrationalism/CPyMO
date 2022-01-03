ddce742de6ea6988f603388b5b7aea4f07d40a2a
许兴逸 <853974536@qq.com>
2022-01-04 00:50:47 +0800

    3ds

--- cpymo-backends/3ds/main.c
+++ cpymo-backends/3ds/main.c
@@ -1,22 +1,54 @@
 #include <3ds.h>
 #include <stdio.h>
-#include <cpymo_gameconfig.h>
-
+#include <cpymo_engine.h>
+#include <citro3d.h>
+#include <citro2d.h>
 #include "select_game.h"
 
-cpymo_gameconfig gameconfig;
+cpymo_engine engine;
+C3D_RenderTarget *screen;
+
+#define SCREEN_WIDTH  400
+#define SCREEN_HEIGHT 240
 
 int main(void) {
 	gfxInitDefault();
 	consoleInit(GFX_BOTTOM, NULL);
-
 	gfxSetDoubleBuffering(GFX_BOTTOM, false);
 
-	const char *gamename = select_game();
+	const char *gamename = "DAICHYAN_s60v3";	//select_game();
 	if (gamename == NULL) {
 		gfxExit();
 		return 0;
 	}
+
+	error_t err = cpymo_engine_init(&engine, "/pymogames/DAICHYAN_s60v3");
+	if (err != CPYMO_ERR_SUCC) {
+		printf("[Error] cpymo_engine_init: %s.", cpymo_error_message(err));
+		gfxExit();
+		return -1;
+	}
+
+	if (!C3D_Init(C3D_DEFAULT_CMDBUF_SIZE)) {
+		cpymo_engine_free(&engine);
+		gfxExit();
+		return -1;
+	}
+
+	if (!C2D_Init(C2D_DEFAULT_MAX_OBJECTS)) {
+		C3D_Fini();
+		cpymo_engine_free(&engine);
+		gfxExit();
+		return -1;
+	}
+
+	C2D_Prepare();
+
+	screen = C2D_CreateScreenTarget(GFX_TOP, GFX_LEFT);
+
+	u32 clr = C2D_Color32(0x00, 0x00, 0xFF, 0xFF);
+	u32 wtf = C2D_Color32(0xFF, 0xFF, 0xFF, 0xFF);
+	
 
 	while (aptMainLoop()) {
 		hidScanInput();
@@ -25,11 +57,22 @@ int main(void) {
 
 		if (kDown & KEY_START) break;
 
-		gfxFlushBuffers();
-		gfxSwapBuffers();
+		{
+			C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
+			C2D_TargetClear(screen, clr);
+
+			C2D_SceneBegin(screen);
+
+			C2D_DrawRectangle(200 - 25, 120 - 25, -1.0f, 50, 50, wtf, wtf, wtf, wtf);
+
+			C3D_FrameEnd(0);
+		}
 
 		gspWaitForVBlank();
 	}
+
+	C2D_Fini();
+	C3D_Fini();
 
 	gfxExit();
 
--- makefiles/3ds/makefile
+++ makefiles/3ds/makefile
@@ -57,7 +57,7 @@ CXXFLAGS	:= $(CFLAGS) -fno-rtti -fno-exceptions -std=gnu++11
 ASFLAGS	:=	-g $(ARCH)
 LDFLAGS	=	-specs=3dsx.specs -g $(ARCH) -Wl,-Map,$(notdir $*.map)
 
-LIBS	:= -lctru -lm
+LIBS	:= -lcitro2d -lcitro3d -lctru -lm
 
 #---------------------------------------------------------------------------------
 # list of directories containing libraries, this must be the top level containing
