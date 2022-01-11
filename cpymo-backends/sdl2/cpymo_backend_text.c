bd735d74e8197821f14b201e9b11104d9723610a
许兴逸 <853974536@qq.com>
2022-01-11 21:37:08 +0800

    Text Optimized

--- cpymo-backends/sdl2/cpymo_backend_text.c
+++ cpymo-backends/sdl2/cpymo_backend_text.c
@@ -26,31 +26,14 @@ error_t cpymo_backend_text_create(
     struct cpymo_backend_text *t = (struct cpymo_backend_text *)malloc(sizeof(struct cpymo_backend_text));
     if (t == NULL) return CPYMO_ERR_OUT_OF_MEM;
 
-    t->width = 1024;
-    t->height = 768;
-    void *screen = malloc(1024 * 768);
-    if (screen == NULL) {
-        free(t);
-        return CPYMO_ERR_OUT_OF_MEM;
-    }
-
-    void *screen2 = malloc(1024 * 768 * 4);
-    if (screen2 == NULL) {
-        free(screen);
-        free(t);
-        return CPYMO_ERR_OUT_OF_MEM;
-    }
-
-    memset(screen, 0, 1024 * 768);
-    memset(screen2, 255, 1024 * 768 * 4);
-
     t->scale = stbtt_ScaleForPixelHeight(&font, single_character_size_in_logical_screen);
     stbtt_GetFontVMetrics(&font, &t->ascent, NULL, NULL);
     t->baseline = t->scale * t->ascent;
 
     float xpos = 0;
-
-    cpymo_parser_stream_span span = cpymo_parser_stream_span_pure(utf8_string);
+    t->height = 0;
+    cpymo_parser_stream_span span_org = cpymo_parser_stream_span_pure(utf8_string);
+    cpymo_parser_stream_span span = span_org;
     while (span.len > 0) {
         uint32_t codepoint = cpymo_parser_stream_span_utf8_try_head_to_utf32(&span);
         int x0, y0, x1, y1;
@@ -59,7 +42,46 @@ error_t cpymo_backend_text_create(
         float x_shift = xpos - (float)floor(xpos);
         stbtt_GetCodepointHMetrics(&font, (int)codepoint, &advance_width, &lsb);
         stbtt_GetCodepointBitmapBoxSubpixel(&font, (int)codepoint, t->scale, t->scale, x_shift, 0, &x0, &y0, &x1, &y1);
-        stbtt_MakeCodepointBitmapSubpixel(&font, (char *)screen + (int)xpos + x0 + (int)(t->baseline + y0) * 1024, x1 - x0, y1 - y0, 1024, t->scale, t->scale, x_shift, 0, (int)codepoint);
+        xpos += (advance_width * t->scale);
+
+        cpymo_parser_stream_span span2 = span;
+        uint32_t next_char;
+        if (next_char = cpymo_parser_stream_span_utf8_try_head_to_utf32(&span2)) {
+            xpos += t->scale * stbtt_GetCodepointKernAdvance(&font, codepoint, next_char);
+        }
+
+        t->height = max(t->height, (y1 - y0) + t->baseline);
+    }
+
+    t->width = (int)ceilf(xpos);
+    
+    void *screen = malloc(t->width * t->height);
+    if (screen == NULL) {
+        free(t);
+        return CPYMO_ERR_OUT_OF_MEM;
+    }
+
+    void *screen2 = malloc(t->width * t->height * 4);
+    if (screen2 == NULL) {
+        free(screen);
+        free(t);
+        return CPYMO_ERR_OUT_OF_MEM;
+    }
+
+    memset(screen, 0, t->width * t->height);
+    memset(screen2, 255, t->width * t->height * 4);
+
+    xpos = 0;
+    span = span_org;
+    while (span.len > 0) {
+        uint32_t codepoint = cpymo_parser_stream_span_utf8_try_head_to_utf32(&span);
+        int x0, y0, x1, y1;
+
+        int advance_width, lsb;
+        float x_shift = xpos - (float)floor(xpos);
+        stbtt_GetCodepointHMetrics(&font, (int)codepoint, &advance_width, &lsb);
+        stbtt_GetCodepointBitmapBoxSubpixel(&font, (int)codepoint, t->scale, t->scale, x_shift, 0, &x0, &y0, &x1, &y1);
+        stbtt_MakeCodepointBitmapSubpixel(&font, (char *)screen + (int)xpos + x0 + (int)(t->baseline + y0) * t->width, x1 - x0, y1 - y0, t->width, t->scale, t->scale, x_shift, 0, (int)codepoint);
         xpos += (advance_width * t->scale);
 
         cpymo_parser_stream_span span2 = span;
@@ -69,10 +91,10 @@ error_t cpymo_backend_text_create(
         }
     }
 
-    cpymo_utils_attach_mask_to_rgba(screen2, screen, 1024, 768);
+    cpymo_utils_attach_mask_to_rgba(screen2, screen, t->width, t->height);
     free(screen);
 
-    error_t err = cpymo_backend_image_load(&t->img, screen2, 1024, 768, cpymo_backend_image_format_rgba);
+    error_t err = cpymo_backend_image_load(&t->img, screen2, t->width, t->height, cpymo_backend_image_format_rgba);
     if (err != CPYMO_ERR_SUCC) {
         free(t);
         return CPYMO_ERR_UNKNOWN;
