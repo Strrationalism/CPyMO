4e43f0c623bf3762a62926224fefe9ac3f3c5154
许兴逸 <853974536@qq.com>
2022-01-17 22:05:19 +0800

    textbox design

---
+++ cpymo/cpymo_text_system.h
@@ -0,0 +1,30 @@
+#ifndef INCLUDE_CPYMO_TEXT_SYSTEM
+#define INCLUDE_CPYMO_TEXT_SYSTEM
+
+#include "cpymo_textbox.h"
+
+struct cpymo_textbox_list {
+	cpymo_textbox textbox;
+	struct cpymo_textbox_list *next;
+};
+
+typedef struct {
+	struct cpymo_textbox_list *textboxes;
+	cpymo_textbox *active_textbox;
+} cpymo_text_system;
+
+void cpymo_text_system_init(cpymo_text_system *);
+void cpymo_text_system_free(cpymo_text_system *);
+
+void cpymo_text_system_draw(const cpymo_text_system *);
+
+error_t cpymo_text_system_new(
+	cpymo_textbox **out,
+	float x, float y,
+	float width, float height,
+	float character_size,
+	cpymo_color col);
+
+void cpymo_text_system_clear(cpymo_text_system *);
+
+#endif
--- cpymo/cpymo_textbox.h
+++ cpymo/cpymo_textbox.h
@@ -9,7 +9,36 @@ typedef struct {
 	float character_size;
 	size_t max_lines;
 	cpymo_backend_text *lines;
-	cpymo_parser_stream_span text;
+	size_t active_line;
+	cpymo_parser_stream_span text_curline_and_remaining;
+	cpymo_color color;
 } cpymo_textbox;
+
+error_t cpymo_textbox_init(
+	cpymo_textbox **out,
+	float x, float y,
+	float width, float height,
+	float character_size,
+	cpymo_color col);
+
+void cpymo_textbox_free(
+	cpymo_textbox *);
+
+void cpymo_textbox_draw(
+	cpymo_textbox *);
+
+void cpymo_textbox_show_next_line(cpymo_textbox *);
+
+void cpymo_textbox_show_next_char(cpymo_textbox *);
+
+bool cpymo_textbox_page_full(cpymo_textbox *);
+
+void cpymo_textbox_show_next_page(cpymo_textbox *);
+
+void cpymo_textbox_clear_page(cpymo_textbox *);
+
+bool cpymo_textbox_all_finished(cpymo_textbox *);
+
+void cpymo_textbox_finalize(cpymo_textbox *);
 
 #endif
