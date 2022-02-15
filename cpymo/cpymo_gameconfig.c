a3470fdb38dd3bd8a518bfbebc9c24ae99baf954
许兴逸 <853974536@qq.com>
2022-02-15 16:33:16 +0800

    FFmpeg is emotional damage!

--- cpymo/cpymo_gameconfig.c
+++ cpymo/cpymo_gameconfig.c
@@ -191,6 +191,8 @@ error_t cpymo_gameconfig_parse(cpymo_gameconfig *out_config, const char *stream,
 	strcpy(out_config->cgprefix, "EV_");
 	out_config->grayselected = 1;
 	out_config->namealign = 1;
+	strcpy(out_config->bgmformat, "mp3");
+	strcpy(out_config->voiceformat, "mp3");
 
 	/*** Default Config End ***/
 
