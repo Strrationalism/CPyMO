65e43f7f10b74067e0da337b3c739331fab4411f
Seng Jik <xc1998@foxmail.com>
2022-07-27 16:09:19 +0800

    Android download ffmpeg.

--- cpymo-backends/android/build-android-ffmpeg.sh
+++ cpymo-backends/android/build-android-ffmpeg.sh
@@ -1,6 +1,7 @@
 cd ffmpeg-android-maker
+curl https://codeload.github.com/FFmpeg/FFmpeg/tar.gz/refs/tags/n5.0.1 --output ffmpeg.tar.gz
 ./ffmpeg-android-maker.sh \
     -android=18 \
-    --source-tar=https://codeload.github.com/FFmpeg/FFmpeg/tar.gz/refs/tags/n5.0.1
-
+    --source-tar=ffmpeg.tar.gz
+rm -rf ffmpeg.tar.gz
 cd ..
