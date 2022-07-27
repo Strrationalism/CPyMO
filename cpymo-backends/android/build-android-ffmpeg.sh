fd491f231793c9415d1a3202b199a990a51318fb
Seng Jik <xc1998@foxmail.com>
2022-07-27 15:48:10 +0800

    fix android ffmpeg url

--- cpymo-backends/android/build-android-ffmpeg.sh
+++ cpymo-backends/android/build-android-ffmpeg.sh
@@ -1,6 +1,6 @@
 cd ffmpeg-android-maker
 ./ffmpeg-android-maker.sh \
     -android=18 \
-    --source-git-tag=n5.0
+    --source-tar=https://codeload.github.com/FFmpeg/FFmpeg/tar.gz/refs/tags/n5.0.1
 
 cd ..
