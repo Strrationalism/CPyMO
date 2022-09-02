db973d4a872f5757d368087995019243ed9e8a5c
Seng Jik <xc1998@foxmail.com>
2022-09-02 23:06:09 +0800

    Wii bug fixed

--- cpymo-backends/sdl1/cpymo_backend_font.c
+++ cpymo-backends/sdl1/cpymo_backend_font.c
@@ -1,8 +1,2 @@
-﻿#include <cpymo_prelude.h>
-
-#ifdef __WII__
-#define GAME_SELECTOR_DIR "sd:/pymogames/"
-#endif
-
-#include "../sdl2/cpymo_backend_font.c"
+﻿#include "../sdl2/cpymo_backend_font.c"
 
