11c4ebf269f24fcba7e698b938991b5dfddd9c51
Seng Jik <xc1998@foxmail.com>
2023-02-02 00:35:48 +0800

    Feature Level >= 1 not compatible DISABLE_AUTOSAVE and LOW_FRAMERATE

--- cpymo/cpymo_prelude.h
+++ cpymo/cpymo_prelude.h
@@ -9,4 +9,12 @@
 #define CPYMO_FEATURE_LEVEL 0
 #endif
 
+#if CPYMO_FEATURE_LEVEL >= 1
+
+#if defined DISABLE_AUTOSAVE || defined LOW_FRAME_RATE
+#error "Feature Level >= 1 not compatible DISABLE_AUTOSAVE and LOW_FRAMERATE."
+#endif
+
+#endif
+
 #endif
