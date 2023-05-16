6032458c49b9e65fd5c149c073911e957e4ef284
许兴逸 <853974536@qq.com>
2023-05-16 21:16:27 +0800

    rewrite cpymo pack in cpymo tool pack api

--- cpymo-tool/cpymo_tool_package.h
+++ cpymo-tool/cpymo_tool_package.h
@@ -1,17 +1,16 @@
 #ifndef INCLUDE_CPYMO_TOOL_PACKAGE
 #define INCLUDE_CPYMO_TOOL_PACKAGE
 
-#include <stdint.h>
 #include <stdio.h>
 #include "../cpymo/cpymo_str.h"
 
 typedef struct {
-    uint32_t max_file_count, current_file_count;
-    uint64_t index_section_start_offset, data_section_start_offset;
+    size_t max_file_count, current_file_count;
+    long index_section_start_offset, data_section_start_offset;
     FILE *stream;
 } cpymo_tool_package_packer;
 
-void cpymo_tool_package_packer_open(
+error_t cpymo_tool_package_packer_open(
     cpymo_tool_package_packer *packer,
     const char *path,
     size_t max_files_count);
