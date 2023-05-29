e2436e091812ebe7cc66e369c8624839107d8071
许兴逸 <853974536@qq.com>
2023-05-30 02:20:18 +0800

    CPyMO Tool Pack API (#43)
    * add cpymo_tool_package_packer api
    
    * rewrite cpymo pack in cpymo tool pack api
    
    * stricter pack api

---
+++ cpymo-tool/cpymo_tool_package.h
@@ -0,0 +1,31 @@
+#ifndef INCLUDE_CPYMO_TOOL_PACKAGE
+#define INCLUDE_CPYMO_TOOL_PACKAGE
+
+#include <stdio.h>
+#include "../cpymo/cpymo_str.h"
+
+typedef struct {
+    size_t max_file_count, current_file_count;
+    long index_section_start_offset, data_section_start_offset;
+    FILE *stream;
+} cpymo_tool_package_packer;
+
+error_t cpymo_tool_package_packer_open(
+    cpymo_tool_package_packer *packer,
+    const char *path,
+    size_t max_files_count);
+
+error_t cpymo_tool_package_packer_add_data(
+    cpymo_tool_package_packer *packer,
+    cpymo_str name,
+    void *data,
+    size_t len);
+
+error_t cpymo_tool_package_packer_add_file(
+    cpymo_tool_package_packer *packer,
+    const char *file);
+
+void cpymo_tool_package_packer_close(
+    cpymo_tool_package_packer *packer);
+
+#endif
