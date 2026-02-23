bde0c8eda032ebf9d13874da8a1c0ec555e59a50
许兴逸 <853974536@qq.com>
2026-02-24 00:48:18 +0800

    升级emscripten端ffmpeg

--- cpymo-backends/sdl2/build-emscripten-ffmpeg.sh
+++ cpymo-backends/sdl2/build-emscripten-ffmpeg.sh
@@ -1,7 +1,7 @@
-curl https://codeload.github.com/FFmpeg/FFmpeg/tar.gz/refs/tags/n5.0.1 --output ffmpeg.tar.gz
+curl https://codeload.github.com/FFmpeg/FFmpeg/tar.gz/refs/tags/n5.1 --output ffmpeg.tar.gz
 tar -xf ffmpeg.tar.gz
 
-cd FFmpeg-n5.0.1
+cd FFmpeg-n5.1
 
 chmod +x ./configure
 
@@ -50,6 +50,6 @@ make install -j
 
 cd ..
 
-rm -rf FFmpeg-n5.0.1 ffmpeg.tar.gz
+rm -rf FFmpeg-n5.1 ffmpeg.tar.gz
 
 
