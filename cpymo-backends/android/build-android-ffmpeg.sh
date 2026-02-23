c5b46e849de2735a9d1ae511a5dfc9be50ba011f
许兴逸 <853974536@qq.com>
2026-02-24 01:09:01 +0800

    Try fix Android CI

--- cpymo-backends/android/build-android-ffmpeg.sh
+++ cpymo-backends/android/build-android-ffmpeg.sh
@@ -1,7 +1,9 @@
+export FFMPEG_EXTRA_CONFIGURE_FLAGS="--disable-doc"
+
 cd ffmpeg-android-maker
 
 ./ffmpeg-android-maker.sh \
     -android=18 \
-    --source-git-tag=n5.0.1
+    --source-git-tag=n5.1
 
 cd ..
