1cc8916480fdb27c8dc0a0a4f33d199956c19ad0
许兴逸 <853974536@qq.com>
2026-02-26 21:22:25 +0800

    更新UWP证书

---
+++ cpymo-backends/uwp/New-Cert.ps1
@@ -0,0 +1,3 @@
+$cert = New-SelfSignedCertificate -Type Custom -KeyUsage DigitalSignature -CertStoreLocation "Cert:\CurrentUser\My" -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}") -Subject "CN=CPyMO, O=CPyMO, C=US" -FriendlyName "CPyMO"
+$password = ConvertTo-SecureString -String "12345" -Force -AsPlainText
+Export-PfxCertificate -cert "Cert:\CurrentUser\My\$($cert.Thumbprint)" -FilePath cert.pfx -Password $password
